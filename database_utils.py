2e2aa3a5641cbec1508aaa9949b1fdcdc73f22b9
Wilson <wilson61696@hotmail.co.uk>
2024-10-21 14:35:34 +0100

    Added functionality to upload data to local database

--- database_utils.py
+++ database_utils.py
@@ -1,5 +1,6 @@
 import yaml
 from sqlalchemy import create_engine
+import pandas as pd
 
 class DatabaseConnector:
 
@@ -8,17 +9,29 @@ class DatabaseConnector:
         with open("db_creds.yaml", "r") as stream:
             creds = yaml.safe_load(stream)
             return creds
+        
+    def read_local_creds(self):
+
+        with open("local_creds.yaml", "r") as stream:
+            creds = yaml.safe_load(stream)
+            return creds
 
     def init_db_engine(self):
         
         creds = self.read_db_creds()
 
-        DATABASE_TYPE = 'postgresql'
-        DBAPI = 'psycopg2'
-
-        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://"
+        engine = create_engine("postgresql+psycopg2://"
                                f"{creds["RDS_USER"]}:{creds["RDS_PASSWORD"]}@"
                                f"{creds["RDS_HOST"]}:{creds["RDS_PORT"]}/{creds["RDS_DATABASE"]}")
         
         return engine
+    
+    def upload_to_db(self, data, table):
 
+        creds = self.read_local_creds()
+
+        engine = create_engine("postgresql+psycopg2://"
+                                f"{creds["RDS_USER"]}:{creds["RDS_PASSWORD"]}@"
+                                f"{creds["RDS_HOST"]}:{creds["RDS_PORT"]}/{creds["RDS_DATABASE"]}")
+        
+        data.to_sql(table, engine, if_exists = "replace")
