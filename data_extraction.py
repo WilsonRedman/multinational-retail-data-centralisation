e6f3bd9e389f1a75abbd5066fa4413ccb63001af
Wilson <wilson61696@hotmail.co.uk>
2024-10-21 14:53:45 +0100

    Added functionality to read pdf data

--- data_extraction.py
+++ data_extraction.py
@@ -1,29 +1,30 @@
 import database_utils
 from sqlalchemy import inspect, text
 import pandas as pd
+import tabula
 
 class DataExtractor:
 
     def __init__(self):
-
         self.dbConnector = database_utils.DatabaseConnector()
         self.engine = self.dbConnector.init_db_engine()
     
     def list_db_tables(self):
-
         inspector = inspect(self.engine)
         return inspector.get_table_names()
     
     def read_rds_table(self, table):
-
         data = pd.read_sql_table(table, self.engine)
         return data
+    
+    def retrieve_pdf_data(self, link):
+        dfs = tabula.read_pdf(link, stream = True, pages = "all", lattice = True)
+        df = pd.concat(dfs, ignore_index = True)
+        return df
+
 
 if __name__ == "__main__":
     ## Code testing the functionality 
     extractor = DataExtractor()
 
-    tables = extractor.list_db_tables()
-    print(tables)
-
-    print(extractor.read_rds_table("legacy_users"))
+    df = extractor.retrieve_pdf_data("https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf")
